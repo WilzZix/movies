614c8df79e91a2738b9d2e4003f43ae10fe956e8
wilzix <nodirbarotov0707@gmail.com>
2024-09-03 18:32:40 +0500

    Home page done

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -58,8 +60,303 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 24,
+              ),
+              Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  image: const DecorationImage(
+                    fit: BoxFit.cover,
+                    image: NetworkImage(
+                      'https://img.freepik.com/free-psd/interior-design-landing-page-template_23-2148663724.jpg?w=1380&t=st=1725283615~exp=1725284215~hmac=bdfcbae6fea688c789a3f7322eae28ba6fda82e8d9f9066067eff271d06dedf7',
+                    ),
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const MovieRatingWidget(
+                      ratingPoint: '8,7',
+                    ),
+                    const Spacer(),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 16,
+                        vertical: 8,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.5),
+                        border: Border.all(
+                          color: Colors.white,
+                        ),
+                        borderRadius: const BorderRadius.only(
+                          bottomLeft: Radius.circular(16),
+                          bottomRight: Radius.circular(16),
+                        ),
+                      ),
+                      child: const Row(
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Oppenheimer',
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(height: 8),
+                              Text(
+                                'History Thriller Drama Mystery',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Row(
+                                children: [
+                                  TextContainerWidget(
+                                    title: '17+',
+                                  ),
+                                  SizedBox(
+                                    width: 8,
+                                  ),
+                                  TextContainerWidget(
+                                    title: '2023',
+                                  ),
+                                  SizedBox(
+                                    width: 8,
+                                  ),
+                                  TextContainerWidget(
+                                    title: '3 hours',
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 32,
+                          ),
+                          AddToWatchListWidget(),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SeeAllMoviesListWidget(
+                title: 'Top Movies Pick',
+                onTap: () {},
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                height: 250,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return const MovieListItem();
+                  },
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              SeeAllMoviesListWidget(
+                title: 'Upcoming Movies',
+                onTap: () {},
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                height: 250,
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) {
+                    return const MovieListItem();
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MovieListItem extends StatelessWidget {
+  const MovieListItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
+            child: Image.network(
+              'https://img.freepik.com/free-psd/interior-design-landing-page-template_23-2148663724.jpg?w=1380&t=st=1725283615~exp=1725284215~hmac=bdfcbae6fea688c789a3f7322eae28ba6fda82e8d9f9066067eff271d06dedf7',
+              width: 150,
+              height: 200,
+              fit: BoxFit.fill,
+            ),
+          ),
+          const SizedBox(
+            height: 8,
+          ),
+          const Text(
+            'Spiderman Across',
+            style: TextStyle(color: Colors.white),
+          ),
+          const SizedBox(
+            height: 4,
+          ),
+          const Text(
+            '2023 PG 2h 20m',
+            style: TextStyle(color: Colors.white),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class SeeAllMoviesListWidget extends StatelessWidget {
+  const SeeAllMoviesListWidget({
+    super.key,
+    required this.title,
+    required this.onTap,
+  });
+
+  final String title;
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(
+          title,
+          style: const TextStyle(color: Colors.white),
+        ),
+        const Spacer(),
+        GestureDetector(
+          onTap: onTap,
+          child: const Text(
+            'See all',
+            style: TextStyle(color: Colors.white),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class MovieRatingWidget extends StatelessWidget {
+  const MovieRatingWidget({
+    super.key,
+    required this.ratingPoint,
+  });
+
+  final String ratingPoint;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        margin: const EdgeInsets.all(4.0),
+        padding: const EdgeInsets.symmetric(horizontal: 4.0),
+        decoration: BoxDecoration(
+            border: Border.all(
+              width: 2,
+              color: Colors.white,
+            ),
+            borderRadius: BorderRadius.circular(8)),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Icon(
+              Icons.star,
+              color: Colors.white,
+              size: 16,
+            ),
+            const SizedBox(
+              width: 4,
+            ),
+            Text(
+              ratingPoint,
+              style: const TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AddToWatchListWidget extends StatelessWidget {
+  const AddToWatchListWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        color: Colors.deepPurple,
+        borderRadius: BorderRadius.circular(
+          6,
+        ),
+      ),
+      child: const Text(
+        'Add to watchlist',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.w400,
+        ),
+      ),
+    );
+  }
+}
+
+class TextContainerWidget extends StatelessWidget {
+  const TextContainerWidget({
+    super.key,
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        shape: BoxShape.rectangle,
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+          color: Colors.white,
+          width: 2,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        child: Text(
+          title,
+          style: const TextStyle(color: Colors.white),
         ),
       ),
     );
