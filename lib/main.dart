f0d577b3a665f395cf15b2367fb417736b9250bc
Nodir Barotov <nodirbek.barotov@opentech.uz>
2024-09-25 11:04:01 +0500

    test deployment

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies/application/actors/actors_bloc.dart';
 import 'package:movies/application/blocs/movies_bloc.dart';
 import 'package:movies/core/network_provider.dart';
-import 'package:movies/presentation/pages/home_page/home_page.dart';
 import 'package:movies/routes/go_router/go_router.dart';
 
 void main() {
